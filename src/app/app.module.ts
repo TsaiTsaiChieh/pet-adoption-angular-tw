3755ca440c1e8a860725c0408e7c9a98a1beee3e
Tsai-Chieh <d0177381@gmail.com>
2023-08-21 21:09:10 +0800

    :construction: use material component and styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,21 @@ import { BrowserModule } from '@angular/platform-browser'
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
-import { HeaderComponent } from './layout/header/header.component';
-import { PetsComponent } from './pages/pets/pets/pets.component';
+import { HeaderComponent } from './layout/header/header.component'
+import { PetsComponent } from './pages/pets/pets/pets.component'
 import { PetCardComponent } from './pages/pets/pet-card/pet-card.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     PetsComponent,
-    PetCardComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     HeaderComponent,
+    PetCardComponent,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/pets/pet-card/pet-card.component.ts
+++ src/app/pages/pets/pet-card/pet-card.component.ts
@@ -1,9 +1,14 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
+import { MatButtonModule } from '@angular/material/button'
+import { MatCardModule } from '@angular/material/card'
+import { MatIconModule } from '@angular/material/icon'
 
 @Component({
   selector: 'app-pet-card',
   templateUrl: './pet-card.component.html',
-  styleUrls: ['./pet-card.component.scss']
+  styleUrls: ['./pet-card.component.scss'],
+  standalone: true,
+  imports: [MatCardModule, MatButtonModule, MatIconModule],
 })
 export class PetCardComponent {
 
