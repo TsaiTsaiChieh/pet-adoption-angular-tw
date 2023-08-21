2a2b06075a65eefe114c33b271feec4fab8ceb04
Tsai-Chieh <d0177381@gmail.com>
2023-08-21 16:34:01 +0800

    :tada: ng g c pages/pets/pet-card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { HeaderComponent } from './layout/header/header.component';
-import { PetsComponent } from './pages/pets/pets/pets.component'
+import { PetsComponent } from './pages/pets/pets/pets.component';
+import { PetCardComponent } from './pages/pets/pet-card/pet-card.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     PetsComponent,
+    PetCardComponent,
   ],
   imports: [
     BrowserModule,
