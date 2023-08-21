52456577ab8c661c99b8f2e5c548db5c5db453d0
Tsai-Chieh <d0177381@gmail.com>
2023-08-21 15:50:54 +0800

    :sparkles: finish header by using Toolbar from material, ref from https://material.angular.io/components/toolbar/overview

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core'
-
+import { MatIconModule } from '@angular/material/icon'
+import { MatToolbarModule } from '@angular/material/toolbar'
 @Component({
-  selector: 'app-header',
+  selector: 'app-layout-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
+  standalone: true,
+  imports: [MatToolbarModule, MatIconModule]
 })
 export class HeaderComponent {
 
