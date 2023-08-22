28e62e57e912f3caca360f94f57348951a4ed834
Tsai-Chieh <d0177381@gmail.com>
2023-08-22 13:46:14 +0800

    :sparkles: add api for query

--- src/app/services/pet.service.ts
+++ src/app/services/pet.service.ts
@@ -1,10 +1,17 @@
+import { HttpClient } from '@angular/common/http'
 import { Injectable } from '@angular/core'
 
 @Injectable({
   providedIn: 'root'
 })
-export class PetsService {
- 
-  constructor() { }
+export class PetService {
+  private baseUrl = 'https://data.coa.gov.tw/Service/OpenData/TransService.aspx?UnitId=QcbUEzN6E6DL&$top=18'
 
+  constructor(private readonly http: HttpClient) { }
+
+  query (filters: PetFilterType) {
+    let url = this.baseUrl
+    filters.page ? url += `&$skip=${filters.page}` : '&$skip=18'
+    return this.http.get<Pet[]>(url)
+  }
 }
