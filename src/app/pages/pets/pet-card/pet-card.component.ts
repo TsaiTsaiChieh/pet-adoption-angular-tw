d1824735a48e3f1e3be99f6589595b61ca1be0bb
Tsai-Chieh <d0177381@gmail.com>
2023-08-22 13:49:03 +0800

    :sparkles: add pet Input

--- src/app/pages/pets/pet-card/pet-card.component.ts
+++ src/app/pages/pets/pet-card/pet-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core'
+import { Component, Input } from '@angular/core'
 import { MatButtonModule } from '@angular/material/button'
 import { MatCardModule } from '@angular/material/card'
 import { MatIconModule } from '@angular/material/icon'
@@ -12,4 +12,6 @@ import { MatIconModule } from '@angular/material/icon'
 })
 export class PetCardComponent {
 
+  @Input()
+  pet: Partial<Pet> = {}
 }
