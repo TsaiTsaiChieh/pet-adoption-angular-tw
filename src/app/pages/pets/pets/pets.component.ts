ccc2a70241d0327a1440f5a276fef8729feb1f81
Tsai-Chieh <d0177381@gmail.com>
2023-08-22 13:48:22 +0800

    :sparkles: load pet data OnInit

--- src/app/pages/pets/pets/pets.component.ts
+++ src/app/pages/pets/pets/pets.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { PetService } from 'src/app/services/pet.service'
 
 @Component({
   selector: 'app-pets',
   templateUrl: './pets.component.html',
-  styleUrls: ['./pets.component.scss']
+  styleUrls: ['./pets.component.scss'],
 })
-export class PetsComponent {
+export class PetsComponent implements OnInit {
+  pets: Pet[] = []
+  constructor(private service: PetService) {}
 
+  ngOnInit(): void {
+    this.service.query({ page: 1 }).subscribe((data) => {
+      this.pets = data
+    })
+  }
 }
