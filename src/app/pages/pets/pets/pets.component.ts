0ad07eeb8ab7d0ea25d8f39823d6fe3a2d0b1129
Tsai-Chieh <d0177381@gmail.com>
2023-08-23 15:30:09 +0800

    :sparkles: import banner component in pets

--- src/app/pages/pets/pets/pets.component.ts
+++ src/app/pages/pets/pets/pets.component.ts
@@ -2,13 +2,14 @@ import { CommonModule, } from '@angular/common'
 import { Component, OnInit, } from '@angular/core'
 import { PetService, } from 'src/app/services/pet.service'
 
+import { BannerComponent, } from '../banner/banner.component'
 import { PetCardComponent, } from '../pet-card/pet-card.component'
 
 @Component({
   selector: 'app-pets',
   templateUrl: './pets.component.html',
   styleUrls: ['./pets.component.scss',],
-  imports: [CommonModule, PetCardComponent,],
+  imports: [CommonModule, PetCardComponent, BannerComponent,],
   standalone: true,
 },)
 export class PetsComponent implements OnInit {
