5cb2ba4bfbd4feb79a716436b77e4fb6d3be577f
Tsai-Chieh <d0177381@gmail.com>
2023-08-23 12:54:44 +0800

    :sparkles: finish petSex pipe

--- src/app/pages/pets/pet-card/pet-sex.pipe.ts
+++ src/app/pages/pets/pet-card/pet-sex.pipe.ts
@@ -1,13 +1,19 @@
-import { Pipe, PipeTransform } from '@angular/core'
+import { Pipe, PipeTransform, } from '@angular/core'
 
 @Pipe({
   name: 'petSex',
-  standalone: true
-})
+  standalone: true,
+},)
 export class PetSexPipe implements PipeTransform {
-
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null
+  
+  transform(sex: AnimalSex,): unknown {
+    switch (sex) {
+      case 'F':
+        return '妹妹'
+      case 'M':
+        return '弟弟'
+      default:
+        return '未知'
+    }
   }
-
 }
