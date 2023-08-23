04c6e38642bbe93b4458b257561c12f9d6884b8f
Tsai-Chieh <d0177381@gmail.com>
2023-08-23 11:17:40 +0800

    :sparkles: petName pipe done and set it 2 standalone

--- src/app/pages/pets/pet-card/pet-name.pipe.ts
+++ src/app/pages/pets/pet-card/pet-name.pipe.ts
@@ -1,12 +1,25 @@
 import { Pipe, PipeTransform } from '@angular/core'
-
+/*
+ * Pet name transfer
+ * Usage:
+ *   color | petName:kind
+*/
 @Pipe({
-  name: 'petName'
+  name: 'petName',
+  standalone: true,
 })
 export class PetNamePipe implements PipeTransform {
-
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null
+  kindTransform (kind: string): string {
+    switch (kind) {
+      case '狗':
+        return '汪'
+      case '貓':
+        return '喵'
+      default:
+        return kind
+    }
   }
-
+  transform (color: string, kind: string): string {
+    return color.replace('色', '') + this.kindTransform(kind)
+  }
 }
