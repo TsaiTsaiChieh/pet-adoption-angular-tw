e07b57712fb7d0bb441c686bc23ab058f7cdde41
Tsai-Chieh <d0177381@gmail.com>
2023-08-23 13:19:01 +0800

    :sparkles: import PetAgePipe

--- src/app/pages/pets/pet-card/pet-card.component.ts
+++ src/app/pages/pets/pet-card/pet-card.component.ts
@@ -4,13 +4,14 @@ import { MatCardModule, } from '@angular/material/card'
 import { MatIconModule, } from '@angular/material/icon'
 import { PetNamePipe, } from './pet-name.pipe'
 import { PetSexPipe, } from './pet-sex.pipe'
+import { PetAgePipe, } from './pet-age.pipe'
 
 @Component({
   selector: 'app-pet-card',
   templateUrl: './pet-card.component.html',
   styleUrls: ['./pet-card.component.scss',],
   standalone: true,
-  imports: [MatCardModule, MatButtonModule, MatIconModule,PetNamePipe, PetSexPipe,],
+  imports: [MatCardModule, MatButtonModule, MatIconModule, PetNamePipe, PetSexPipe, PetAgePipe,],
 },)
 export class PetCardComponent {
 
