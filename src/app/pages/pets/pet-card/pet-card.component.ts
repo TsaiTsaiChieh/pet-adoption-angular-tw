f08992203b6a53ad3645d7117f1fb5480d0c7c14
Tsai-Chieh <d0177381@gmail.com>
2023-08-23 12:55:04 +0800

    :sparkles: import PetSexPipe

--- src/app/pages/pets/pet-card/pet-card.component.ts
+++ src/app/pages/pets/pet-card/pet-card.component.ts
@@ -1,16 +1,17 @@
-import { Component, Input } from '@angular/core'
-import { MatButtonModule } from '@angular/material/button'
-import { MatCardModule } from '@angular/material/card'
-import { MatIconModule } from '@angular/material/icon'
-import { PetNamePipe } from './pet-name.pipe'
+import { Component, Input, } from '@angular/core'
+import { MatButtonModule, } from '@angular/material/button'
+import { MatCardModule, } from '@angular/material/card'
+import { MatIconModule, } from '@angular/material/icon'
+import { PetNamePipe, } from './pet-name.pipe'
+import { PetSexPipe, } from './pet-sex.pipe'
 
 @Component({
   selector: 'app-pet-card',
   templateUrl: './pet-card.component.html',
-  styleUrls: ['./pet-card.component.scss'],
+  styleUrls: ['./pet-card.component.scss',],
   standalone: true,
-  imports: [MatCardModule, MatButtonModule, MatIconModule,PetNamePipe],
-})
+  imports: [MatCardModule, MatButtonModule, MatIconModule,PetNamePipe, PetSexPipe,],
+},)
 export class PetCardComponent {
 
   @Input()
