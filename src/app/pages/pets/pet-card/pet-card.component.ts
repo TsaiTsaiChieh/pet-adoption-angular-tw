681560c8f88143227f08244da34eaf1ea7c5e05f
Tsai-Chieh <d0177381@gmail.com>
2023-08-23 11:18:55 +0800

    :sparkles: import petName pipe

--- src/app/pages/pets/pet-card/pet-card.component.ts
+++ src/app/pages/pets/pet-card/pet-card.component.ts
@@ -2,13 +2,14 @@ import { Component, Input } from '@angular/core'
 import { MatButtonModule } from '@angular/material/button'
 import { MatCardModule } from '@angular/material/card'
 import { MatIconModule } from '@angular/material/icon'
+import { PetNamePipe } from './pet-name.pipe'
 
 @Component({
   selector: 'app-pet-card',
   templateUrl: './pet-card.component.html',
   styleUrls: ['./pet-card.component.scss'],
   standalone: true,
-  imports: [MatCardModule, MatButtonModule, MatIconModule],
+  imports: [MatCardModule, MatButtonModule, MatIconModule,PetNamePipe],
 })
 export class PetCardComponent {
 
