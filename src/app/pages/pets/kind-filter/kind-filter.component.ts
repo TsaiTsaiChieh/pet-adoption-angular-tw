a8204728430de6e96707e98b0a1018e0c4c55d4a
Tsai-Chieh <d0177381@gmail.com>
2023-08-24 17:06:34 +0800

    :tada: ng g c pages/pets/kindFilter  --standalone

---
+++ src/app/pages/pets/kind-filter/kind-filter.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule, } from '@angular/common'
+import { Component, } from '@angular/core'
+
+@Component({
+  selector: 'app-kind-filter',
+  standalone: true,
+  imports: [CommonModule,],
+  templateUrl: './kind-filter.component.html',
+  styleUrls: ['./kind-filter.component.scss',],
+},)
+export class KindFilterComponent {
+
+}
