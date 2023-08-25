9f649ac76fa2b44357260dd4eee8da11e0e5a191
Tsai-Chieh <d0177381@gmail.com>
2023-08-25 17:23:08 +0800

    :tada: ng g c pages/pets/ppaginator --standalone

---
+++ src/app/pages/pets/ppaginator/ppaginator.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-ppaginator',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './ppaginator.component.html',
+  styleUrls: ['./ppaginator.component.scss']
+})
+export class PpaginatorComponent {
+
+}
