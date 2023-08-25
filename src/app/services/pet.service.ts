faec05e9b425b28d3d77898f15bd999ceecd853a
Tsai-Chieh <d0177381@gmail.com>
2023-08-25 16:45:58 +0800

    :sparkles: and kind filter in query func and format

--- src/app/services/pet.service.ts
+++ src/app/services/pet.service.ts
@@ -3,15 +3,16 @@ import { Injectable, } from '@angular/core'
 
 @Injectable({
   providedIn: 'root',
-},)
+})
 export class PetService {
   private baseUrl = 'https://data.coa.gov.tw/Service/OpenData/TransService.aspx?UnitId=QcbUEzN6E6DL&$top=18'
 
-  constructor(private readonly http: HttpClient,) { }
+  constructor(private readonly http: HttpClient) { }
 
-  query (filters: PetFilterType,) {
+  query (filters: PetFilterType) {
     let url = this.baseUrl
     filters.page ? url += `&$skip=${filters.page}` : '&$skip=18'
-    return this.http.get<Pet[]>(url,)
+    filters.kind ? url += `&animal_kind=${filters.kind}` : undefined
+    return this.http.get<Pet[]>(url)
   }
 }
